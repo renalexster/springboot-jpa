aad93a93916433e2492d4256fcb5021fe37252a1
Renato Campelo <renalexster@gmail.com>
2020-02-02 00:36:51 -0500

    delete wrong sql

--- src/main/resources/db/V1_2__Dummy_data_account.sql
+++
@@ -1,2 +0,0 @@
-insert into account (account_number, account_holder_id, created_at) values ('1323-12x', 1, now());
-insert into account (account_number, account_holder_id, created_at) values ('3213x', 2, now());
