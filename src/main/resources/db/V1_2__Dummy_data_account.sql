66b822a413a119a1f1c81650032930603112a311
Renato Campelo <renalexster@gmail.com>
2020-02-02 00:36:21 -0500

    flyway change

---
+++ src/main/resources/db/V1_2__Dummy_data_account.sql
@@ -0,0 +1,2 @@
+insert into account (account_number, account_holder_id, created_at) values ('1323-12x', 1, now());
+insert into account (account_number, account_holder_id, created_at) values ('3213x', 2, now());
